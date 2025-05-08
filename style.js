b16ddc937e13c13afa3ed8019cd6de4a7b6b3531
TylerYang123 <Tyler.yang24@student.cart.org>
2025-05-07 22:20:16 -0700

    good work tyler

--- style.js
+++ style.js
@@ -5,3 +5,11 @@ const navLinks = document.getElementById('nav-links');
 mobileMenu.addEventListener('click', () => {
     navLinks.classList.toggle('active');
 });
+function openPopup() {
+    document.getElementById("popup").style.display = "flex";
+  }
+  
+  function closePopup() {
+    document.getElementById("popup").style.display = "none";
+  }
+  
